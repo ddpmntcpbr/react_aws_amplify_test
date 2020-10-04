64be05afde4096a6b44894e9ff497bc3f9cb80fd
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-10-04 22:51:10 +0900

    [Fix] metaタグ情報の修正

--- front/src/templates/TopPage.jsx
+++ front/src/templates/TopPage.jsx
@@ -10,17 +10,17 @@ const TopPage = () => {
   return (
     <Container>
       <Helmet
-        title="TopPage"
+        title={'Hello World'}
         meta={[
-          { name: 'twitter:card', content: 'summary' },
-          { name: 'twitter:title', content: 'TopPage' },
-          { name: 'twitter:description', content: 'description of TopPage' },
-          { name: 'twitter:image', content: 'https://global.canon/ja/environment/bird-branch/photo-gallery/kijibato/img/gallery-kijibato-main@2x.jpg' },
-          { property: 'og:title', content: 'TopPage' },
-          { property: 'og:type', content: 'website' },
-          { property: 'og:url', content: 'https://global.canon/ja/environment/bird-branch/photo-gallery/kijibato/index.html' },
+          { name: 'twitter:card', content: "summary_large_image" },
+          // { name: 'twitter:title', content: 'Yomukatsu!' },
+          // { name: 'twitter:description', content: 'description of TopPage' },
+          // { name: 'twitter:image', content: 'https://global.canon/ja/environment/bird-branch/photo-gallery/kijibato/img/gallery-kijibato-main@2x.jpg' },
+          { property: 'og:title', content: 'TopPageです' },
+          // { property: 'og:type', content: 'website' },
+          // { property: 'og:url', content: 'https://master.d11sevmfggnvt.amplifyapp.com/' },
           { property: 'og:image', content: 'https://global.canon/ja/environment/bird-branch/photo-gallery/kijibato/img/gallery-kijibato-main@2x.jpg' },
-          { property: 'og:description', content: 'description of Home' },
+          { property: 'og:description', content: 'TopPageの詳細です' },
         ]}
       />
       <Typography>かんたん！5秒で登録！</Typography>
