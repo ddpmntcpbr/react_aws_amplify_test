afe47acc681bd6c7d10715bf077d700dd3eb4774
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-10-04 20:23:45 +0900

    [Add] front front2を追加

--- front2/src/templates/TopPage.jsx
+++ front2/src/templates/TopPage.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import {useDispatch} from 'react-redux'
-import {signIn} from "reducks/users/operations"
-import {TwitterLoginButton} from "front/src/components/UIkit"
+import {PrimaryButton} from "../components/UIkit"
 
 const TopPage = () => {
-  const dispatch = useDispatch()
 
   return (
     <Container>
       <Typography>かんたん！5秒で登録！</Typography>
-      <TwitterLoginButton
-        label={"Twitter ログイン / 新規登録"}
-        onClick={() => dispatch(signIn())}
+      <PrimaryButton
+        label={"ボタン"}
+        onClick={() => console.log("Clicked!")}
       />
     </Container>
   );
